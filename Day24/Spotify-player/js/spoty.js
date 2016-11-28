cc02c4d5977675728f7a0ceafa752e55ae0859c4
Juan Pablo OLIVADOTI <jp.olivadoti@gmail.com>
2016-11-28 13:47:07 +0100

    Sunday PUSH night

--- Day24/Spotify-player/js/spoty.js
+++ Day24/Spotify-player/js/spoty.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 	$('#search-btn').click(function(event) {
 		$('#results').html("");
-		$('section.information').css('display','block');
+		// $('section.information').css('display','block');
 
 		console.log("Hi, there!");
 		event.preventDefault();
@@ -27,45 +27,66 @@ function handleErrors(errors){
 
 function showAll(response) {
 
-	$('#console-btn').toggleClass("disabled");
+	$('#console-btn').removeClass("disabled");
+	$('#songsCovers').empty();
 
+	var info = response.tracks.items;
 
-	var info = response.tracks.items[0];
-
-	// console.log(info);
-	// console.log("Hi, there!");
 	console.log(response.tracks);
 
-	$('#author').text(info.artists[0].name);
-	$('#modalAuthor').attr('value',info.artists[0].id); 
-	$('#modalAuthor').text(info.artists[0].name);
-	$('#songTitle').text(info.name);
-	$('#trkUrl').attr('src', info.preview_url);
-	$('#trkImg').attr('src', info.album.images[0].url);
+	console.log(info);
 
-	var authorId = info.artists[0].id;
-	
-	window.localStorage.setItem("author", authorId);
+	$('#author').text(info[0].artists[0].name);
+	$('#modalAuthor').attr('value',info[0].artists[0].id); 
+	$('#modalAuthor').text(info[0].artists[0].name);
+	$('#songTitle').text(info[0].name);
+	$('#trkUrl').attr('src', info[0].preview_url);
+	$('#trkImg').attr('src', info[0].album.images[0].url);
 
-	var moreSongs = response.tracks.items;
+	info.forEach(function(element){
 
-	moreSongs.forEach(function(element){
+		var liElement =`
+			<div class="col-sm-2">
+				<div class="imgBox">
+					<a class="thumbnails" data-ref="${element.href}">
+						<img class="imgThumbnail" src="${element.album.images["0"].url}" alt="">
+					</a>
+				</div>
+			  <h4 class="songTitle" id="songTitle">${element.name}</h4>
+			  <p data-toggle="modal" data-target="#myModal" value=""${element.artists[0].name}>${element.artists[0].name}</p>
+			</div>
+			`;
 
-		var songElement = `
-		<li><a id="thisSong">${element.name} - by ${element.artists["0"].name}</a></li>`;
+		$('#songsCovers').append(liElement);
 
-		$('ul.songList').append(songElement);
-
-		console.log(element.name);
 
 	});
 
+
+	// MODAL info
+	var authorId = info[0].artists[0].id;
+	window.localStorage.setItem("author", authorId);
+	console.log(authorId);
+
 }
+
+$('thumbnails').click(function(event){
+	event.preventDefault();
+	
+	$.ajax ({
+		type: "GET",
+		url: $('.thumbnails').val(),
+		success: showAll,
+		error: handleErrors
+	});
+
+});
 
 //============== MODAL "GET" INFO
 
 $('#author').click(function(event){
 		// $('.js-modal').modal("show");
+	event.preventDefault();
 	var authorId = window.localStorage.author;
 	console.log(authorId);
 
@@ -79,15 +100,14 @@ $('#author').click(function(event){
 //============== MODAL INFO print
 
 function showModal(response){
-	// console.log(response);
+	console.log(response);
 
 	$('#modalAuthor').text(response.name);
-	$('#genres').text(response.genres.join(", ").capitalized);
+	$('#genres').text(response.genres.join(", "));
 	$('#followers').text(response.followers.total);
 	$('#popularity').attr('value', response.popularity);
 	$('#author-photo').attr('src', response.images[0].url);
 
-	console.log(response);
 }
 
 //============== PLAY BUTTON trigger
@@ -119,5 +139,15 @@ function printTime () {
 $('#trkUrl').on('timeupdate', printTime);
 
 
+//======= NAVBAR ANIMATION
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1){  
+        $('header').addClass("sticky");
+    }
+    else{
+        $('header').removeClass("sticky");
+    }
+});
 
 }); // == DOCUMENT.ready end
